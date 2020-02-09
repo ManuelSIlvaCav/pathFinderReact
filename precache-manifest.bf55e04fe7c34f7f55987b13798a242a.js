b655b43742c9ca4f8fe93340ea386191254da689
ManuelSIlvaCav <msilvacav@gmail.com>
2020-02-09 12:04:07 -0300

    Updates

---
+++ precache-manifest.bf55e04fe7c34f7f55987b13798a242a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e7a09b373a95ec11a56c10e777b026",
+    "url": "/pathfinder/index.html"
+  },
+  {
+    "revision": "bfbbd46c85196e14e7b5",
+    "url": "/pathfinder/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "748ed9be5a2164ba59f5",
+    "url": "/pathfinder/static/css/main.bcde3d13.chunk.css"
+  },
+  {
+    "revision": "bfbbd46c85196e14e7b5",
+    "url": "/pathfinder/static/js/2.e2620278.chunk.js"
+  },
+  {
+    "revision": "0c74b13391a5594258a49f4df151b4d3",
+    "url": "/pathfinder/static/js/2.e2620278.chunk.js.LICENSE"
+  },
+  {
+    "revision": "748ed9be5a2164ba59f5",
+    "url": "/pathfinder/static/js/main.9e628613.chunk.js"
+  },
+  {
+    "revision": "2a270c0899af254e1d7b",
+    "url": "/pathfinder/static/js/runtime-main.c7099aa2.js"
+  }
+]);
