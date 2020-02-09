f8ee0f35c7fff3a03838e7534447f31355da2294
ManuelSIlvaCav <msilvacav@gmail.com>
2020-02-09 12:18:54 -0300

    Updates

---
+++ precache-manifest.337548542c0a4c490a3b3488dc5daf1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "549abfbfe4cc611e9c525ff45ca7117e",
+    "url": "/pathFinderReact/index.html"
+  },
+  {
+    "revision": "bfbbd46c85196e14e7b5",
+    "url": "/pathFinderReact/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "0af834d32d5f7ccda732",
+    "url": "/pathFinderReact/static/css/main.bcde3d13.chunk.css"
+  },
+  {
+    "revision": "bfbbd46c85196e14e7b5",
+    "url": "/pathFinderReact/static/js/2.e2620278.chunk.js"
+  },
+  {
+    "revision": "0c74b13391a5594258a49f4df151b4d3",
+    "url": "/pathFinderReact/static/js/2.e2620278.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0af834d32d5f7ccda732",
+    "url": "/pathFinderReact/static/js/main.930154a7.chunk.js"
+  },
+  {
+    "revision": "c1ee7e4ca52adf501a9b",
+    "url": "/pathFinderReact/static/js/runtime-main.b6f2bba8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pathfinder/precache-manifest.bf55e04fe7c34f7f55987b13798a242a.js"
+  "/pathFinderReact/precache-manifest.337548542c0a4c490a3b3488dc5daf1c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pathfinder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pathFinderReact/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
